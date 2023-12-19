8f1bbf912abfb536e0ac5c65abf909340391b197
Bartłomiej Kudłacik <66065145+julianjunior07@users.noreply.github.com>
2023-12-19 17:58:28 +0100

    ploty i płoty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from read_file import *
+from model import *
 
 read_from_file()
 
--- read_file.py
+++ read_file.py
@@ -1,6 +1,11 @@
+from matplotlib import pyplot as plt
+import numpy as np
 
 measurements = []
+
+#odczyt z pliku
 def read_from_file():
+    #odczyt formatu danych, node początkowy i końcowy
     file1 = open("data/1.txt")
     
     format = file1.readline()
@@ -12,10 +17,24 @@ def read_from_file():
     node_destination = file1.readline()
     print(node_destination)
     
+    #odczyt wartosci bitrate 
     for line in file1:
         measurements.append(line.rstrip())
+    #jesli chcemy tylko okreslona liczbe
+    #for i in range(1000):
+    #    line = next(file1).strip()
+    #    measurements.append(line)
+        
+    #konwersja na floaty
+    for i in range(0, len(measurements)):
+        measurements[i] = float(measurements[i])
         
     #print(measurements)
 
-
     file1.close()
+    
+    #pokazanie wykresiku
+    plt.plot(measurements)
+    plt.xlabel('number')
+    plt.ylabel('bitrate')
+    plt.show()
